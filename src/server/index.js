4dc6db63bc74c6a0aa2b191ce3d4cc1edec84248
tomcatbuzz <tomcatbuzz@yahoo.com>
2018-04-16 19:17:44 -0400

    error

--- src/server/index.js
+++ src/server/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3231;
 
 const SocketManager = require('./SocketManager')
 
-app.use(express.static(__dirname + 'build'))
+app.use(express.static(__dirname + '/public'))
 
 io.on('connection', SocketManager)
 
