58157823816b579f00ba0e57eff4a566985b033b
tomcatbuzz <tomcatbuzz@yahoo.com>
2018-04-16 17:12:46 -0400

    fixed server issue

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,22 @@
-const express = require('express')
+const express = require('express');
+const path = require ('path');
 
 const app = express()
 const server = require('http').Server(app)
-const io = module.exports.io = require('socket.io')(app)
 
-const PORT = process.env.PORT || 3231
+const io = module.exports.io = require('socket.io')(server)
+
+
+
+const port = process.env.PORT || 8080;
 
 const SocketManager = require('./SocketManager')
 
-app.use(express.static(__dirname + '/../../build'))
+app.use(express.static(path.join(__dirname, '/../../build')))
 
 io.on('connection', SocketManager)
 
-server.listen(PORT, () => {
-  console.log("Connected to port:" + PORT);
+server.listen(port, () => {
+  if (port === 8080) console.log(`Server started on http://localhost:${port}`);
+  else console.log(`Server started on ${port}`);
 });
